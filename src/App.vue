ffba507ea7efe2326cb4afe8f9b0728a36a67b4c
IgnatDubovskiy <57371070+IgnatDubovskiy@users.noreply.github.com>
2020-10-15 08:26:44 +0300

    finished vue-slider-rgb with dynamic changing color

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" v-bind:style="styles">
+    <h1 :style="styles">Slider RGB</h1>
+    <ColorRange @change-color="changeColor" :color="colors[0]"/>
+    <ColorRange @change-color="changeColor" :color="colors[1]"/>
+    <ColorRange @change-color="changeColor" :color="colors[2]"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ColorRange from '@/components/ColorRange';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ColorRange
+  },
+  methods: {
+    makeColorStr(
+        R = this.components.R,
+        G = this.components.G,
+        B = this.components.B
+    ) {
+      return `rgb(${R}, ${G}, ${B})`;
+    },
+    changeColor(val, comp) {
+      this.components[comp] = val;
+      this.bg = this.makeColorStr();
+      this.styles.backgroundColor = this.bg;
+    }
+  },
+  data() {
+    return {
+      colors: ['R', 'G', 'B'],
+      components: {
+        R: 255,
+        G: 255,
+        B: 255
+      },
+      styles: {
+        backgroundColor: '#fff'
+      }
+    }
   }
 }
 </script>
@@ -24,5 +52,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: #fff;
 }
 </style>
---
+++ src/components/ColorRange.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <p>{{this.color}}: <label>
+      <input
+          type="number"
+          v-model="colorComponent"
+          @change="changeColorComponent"
+          @input="changeColorComponent"
+          min="0"
+          max="255"
+      >
+    </label></p>
+    <label>
+      <input
+          type="range"
+          v-model="colorComponent"
+          @change="changeColorComponent"
+          @input="changeColorComponent"
+          min="0"
+          max="255"
+      >
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      required: true
+    }
+  },
+  name: 'ColorRange',
+  data() {
+    return {
+      colorComponent: '255',
+    }
+  },
+   methods: {
+     changeColorComponent() {
+       let N = this.colorComponent
+       this.$emit('change-color', N, this.color)
+     }
+   }
+};
+</script>
+
+<style scoped>
+input[type=number] {
+  width: 40px;
+}
+</style>
